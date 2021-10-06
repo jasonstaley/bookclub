9fed51edb213402576ec6f5adf8c5b3ad44d4c29
jasonstaley <jestaley@my365.bellevue.edu>
2021-10-06 16:42:16 -0500

    Assignment 6.2 – Access Management

---
+++ jstaley/src/main/java/com/bookclub/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+/*
+Student:	    Jason Staley
+Date: 		    10/6/2021
+Assignment: 	Assignment 6.2 – Access Management
+File Name: 	    SecurityConfig.java
+
+University:	    Bellevue University
+Class:		    CIS530-T301 Server-Side Development (2221-1)
+Professor: 	    Richard Lomax
+
+Citations:
+Lomax, R., Krasso, R. (2021). CIS 530 Server-Side Development. Bellevue University.
+Modified by J. Staley 2021
+*/
+package com.bookclub.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web
+        .configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web
+        .configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth
+            .inMemoryAuthentication()
+            .withUser("user").password(encoder.encode("password")).roles("USER")
+            .and()
+            .withUser("jasons").password(encoder.encode("october")).roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .permitAll();
+    }
+}
---
+++ jstaley/src/main/java/com/bookclub/security/SecurityController.java
@@ -0,0 +1,43 @@
+/*
+Student:	    Jason Staley
+Date: 		    10/6/2021
+Assignment: 	Assignment 6.2 – Access Management
+File Name: 	    SecurityController.java
+
+University:	    Bellevue University
+Class:		    CIS530-T301 Server-Side Development (2221-1)
+Professor: 	    Richard Lomax
+
+Citations:
+Lomax, R., Krasso, R. (2021). CIS 530 Server-Side Development. Bellevue University.
+Modified by J. Staley 2021
+*/
+package com.bookclub.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SecurityController {
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(path = "/logout",  method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
+    }
+}
